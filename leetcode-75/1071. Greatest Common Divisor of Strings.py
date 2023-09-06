a90986a45a7649660197618cf735c39a6c4de420
chaitanya <chaitanya_d@rollingarrays.com>
2023-09-06 10:19:15 +0530

    LT-1071 |  ADDED :  Solution

--- leetcode-75/1071. Greatest Common Divisor of Strings.py
+++ leetcode-75/1071. Greatest Common Divisor of Strings.py
@@ -1,27 +1,18 @@
 # 1071. Greatest Common Divisor of Strings
 class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
-        gcd_str1 = self.get_gcd_element(str1)
-        print('gcd_str1', gcd_str1)
-        gcd_str2 = self.get_gcd_element(str2)
-        print('gcd_str2', gcd_str2)
-        if gcd_str1 == gcd_str2:
-            return gcd_str1
-        return ''
+        len1, len2 = len(str1), len(str2)
 
-    def get_gcd_element(self, strs: str):
-        prev_x_str = ''
-        x_str = ''
-        max_count = 0
-        for char in strs:
-            prev_x_str = x_str
-            x_str += char
-            count = strs.count(x_str)
-            if max_count <= count:
-                max_count = count
-            else:
-                return prev_x_str
-        return x_str
+        def is_divisor(l):
+            if len1 % l or len2 % l:
+                return False
+            factor1, factor2 = len1 // l, len2 // l
+            return str1[:l] * factor1 == str1 and str1[:l] * factor2 == str2
+
+        for l in range(min(len1, len2), 0, -1):
+            if is_divisor(l):
+                return str1[:l]
+        return ''
 
 
 if __name__ == "__main__":
